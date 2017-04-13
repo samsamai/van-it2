8e1ddfffc80ee7585729be940cd4484ffef39a86
Sam S <sam@studio2pi.com.au>
2017-04-13 10:28:27 +0000

    Install rails5

---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+  get 'landing_page/show'
+  root to: 'landing_page#show'
+  
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
